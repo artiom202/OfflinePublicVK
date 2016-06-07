4afe69881634077c45becbca20b35864b9e3e66f
artiom202 <artiomkolomiets@gmail.com>
2016-06-07 08:04:36 +0000

    Add files via upload

---
+++ viewer.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+#Imports
+
+import time
+from bottle import run, route, template, post, request, get, static_file, redirect
+from main import Post, get_content, Comments
+
+#/Imports
+
+# Static Routes
+
+
+@get('/<filename:re:.*\.js>')
+def javascripts(filename):
+    return static_file(filename, root='static/js')
+
+
+@get('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='static/css')
+
+
+@get('/<filename:re:.*\.(jpg|png|gif|ico)>')
+def images(filename):
+    return static_file(filename, root='static/img')
+
+
+@get('/<filename:re:.*\.(eot|ttf|woff|svg)>')
+def fonts(filename):
+    return static_file(filename, root='static/fonts')
+
+
+@get('/')
+def index():
+    g_id = request.forms.get("g_id")
+    return template('index.html', g_id=g_id)
+
+
+@route('/<g_id>')
+def pabl(g_id):
+    get_content(g_id)
+    ids = []
+    for post in Post.select().where(Post.group_id == g_id):
+        if post.id in ids:
+            continue
+        else:
+            ids.append(post.id)
+    return template('pabl.html', ids=ids, Comments=Comments)
+
+
+run(host='localhost', port=9999, debug=True)
+
