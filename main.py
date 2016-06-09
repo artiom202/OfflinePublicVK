3875a1a047ad728eb207037f84b976abf5b26b55
CatKasha <patrik148500@gmail.com>
2016-06-10 02:44:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -26,17 +26,17 @@ class Comments(Model):
 
 
 def download(pic, id):
-	resource = urlopen(pic)
-	if platform.system() == 'Widnows':
-		os_dir = '\\'
-	else:
-	    os_dir = '/'
-	img_dir = 'static' + os_dir + 'img' + os_dir
-	if not os.access(img_dir,os.F_OK):
-		os.makedirs(img_dir)
-	out = open(img_dir + str(id) + '_' + 'pic' + '.jpg', 'wb')
-	out.write(resource.read())
-	out.close()
+    resource = urlopen(pic)
+    if platform.system() == 'Widnows':
+        os_dir = '\\'
+    else:
+        os_dir = '/'
+    img_dir = 'static' + os_dir + 'img' + os_dir
+    if not os.access(img_dir,os.F_OK):
+        os.makedirs(img_dir)
+    out = open(img_dir + str(id) + '_' + 'pic' + '.jpg', 'wb')
+    out.write(resource.read())
+    out.close()
 
 
 def get_content(g_id):
