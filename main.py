ac275b6539f90a8b55473335cbb52630255a7c82
artiom202 <artiomkolomiets@gmail.com>
2016-08-16 14:09:59 +0300

    Update main.py
    Рабочий get_all_content, в основе лежит test_get

--- main.py
+++ main.py
@@ -39,63 +39,83 @@ def download(pic, id, pic_id):
     out.close()
     Pic.create(id=pic_id)
 
+
 def get_all_content(g_id):
     session = vk.Session()
     api = vk.API(session)
 
     group_id = '-' + g_id
-    ofset = 1
+    offset = 1
     while 1:
-        group_posts = api.wall.get(owner_id=group_id, offset=ofset, count=50)
-        if len(group_posts) < 11:
-            text = group_posts.get('text')
-            id = group_posts.get('id')
-            pic = group_posts.get('attachments')[0].get('photo').get('src_big')
-            if id in [post.id for post in Post.select()]:
-                continue
-            else:
-                download(pic, id)
-                post_comments = api.wall.getComments(owner_id=group_id, post_id=id, need_likes=True)
-                for i in range(len(post_comments)-1):
-                    comment = post_comments[1:len(post_comments)][i]
-                    if comment.get('likes').get('count') > 1:
-                        if 'https://vk.com' in comment.get('text'):
+        try:
+            group_posts = api.wall.get(owner_id=group_id, offset=offset, count=50)
+            if len(group_posts) < 51:
+                for group_post in group_posts[1:len(group_posts)]:
+                    try:
+                        text = group_post.get('text')
+                        id = group_post.get('id')
+                        if id in [post.id for post in Post.select()]:
                             continue
-                        else:
-                            if not '[id' in comment.get('text'):
-                                Comments.create(text=comment.get('text'), post_id=id)
+                        for attachment in group_post.get('attachments'):
+
+                            if attachment.get('type') == 'photo':
+                                pic = attachment.get('photo').get('src_big')
+                                pic_id = attachment.get('photo').get('pid')
+                                download(pic, id, pic_id)
                             else:
                                 continue
-                Post.create(text=text, pic_id=id, id=id, group_id=g_id)
-            sleep(30)
-            break
-        else:
-            for group_post in group_posts[1:len(group_posts)]:
-                text = group_post.get('text')
-                id = group_post.get('id')
-                pic = group_post.get('attachments')[0].get('photo').get('src_big')
-                if id in [post.id for post in Post.select()]:
-                    continue
-                else:
-                    download(pic, id)
-                    post_comments = api.wall.getComments(owner_id=group_id, post_id=id, need_likes=True)
-                    for i in range(len(post_comments)-1):
-                        comment = post_comments[1:len(post_comments)][i]
-                        if comment.get('likes').get('count') > 1:
-                            if 'https://vk.com' in comment.get('text'):
-                                continue
-                            else:
-                                if not '[id' in comment.get('text'):
-                                    Comments.create(text=comment.get('text'), post_id=id)
-                                else:
+                        post_comments = api.wall.getComments(owner_id=group_id, post_id=id, need_likes=True)
+                        for i in range(len(post_comments) - 1):
+                            comment = post_comments[1:len(post_comments)][i]
+                            if comment.get('likes').get('count') > 1:
+                                if 'https://vk.com' in comment.get('text'):
                                     continue
+                                else:
+                                    if not '[id' in comment.get('text'):
+                                        Comments.create(text=comment.get('text'), post_id=id)
+                                    else:
+                                        continue
 
-                    Post.create(text=text, pic_id=id, id=id, group_id=g_id)
+                        Post.create(text=text, id=id, group_id=g_id)
+                    except Exception as err:
+                        print(err)
+                break
+            else:
+                print('else')
+                offset += 50
+                for group_post in group_posts[1:len(group_posts)]:
+                    try:
+                        text = group_post.get('text')
+                        id = group_post.get('id')
+                        if id in [post.id for post in Post.select()]:
+                            continue
+                        for attachment in group_post.get('attachments'):
 
-                sleep(30)
+                            if attachment.get('type') == 'photo':
+                                pic = attachment.get('photo').get('src_big')
+                                pic_id = attachment.get('photo').get('pid')
+                                download(pic, id, pic_id)
+                            else:
+                                continue
+                        post_comments = api.wall.getComments(owner_id=group_id, post_id=id, need_likes=True)
+                        for i in range(len(post_comments) - 1):
+                            comment = post_comments[1:len(post_comments)][i]
+                            if comment.get('likes').get('count') > 1:
+                                if 'https://vk.com' in comment.get('text'):
+                                    continue
+                                else:
+                                    if not '[id' in comment.get('text'):
+                                        Comments.create(text=comment.get('text'), post_id=id)
+                                    else:
+                                        continue
+
+                        Post.create(text=text, id=id, group_id=g_id)
+                    except Exception as err:
+                        print(err)
                 continue
-            print('iterration++')
-        ofset += 10
+        except Exception as err:
+            print('Error')
+            print(err)
         
 
 def get_content(g_id):
