f17148bcab847828a40581d191b525ed9e5b7b05
Sandmonster <nelex111@gmail.com>
2016-09-27 20:54:04 +0300

    Back button added

--- main.py
+++ main.py
@@ -132,48 +132,6 @@ def get_all_content(g_id):
             print('Error')
             print(err)
 
-
-# !!! ТЕСТОВАЯ ФУНКЦИЯ ДЛЯ ОТОБРАЖЕНИЯ КАРТИНОК БЕЗ ИХ ЗАГРУЗКИ !!!#
-def url_get(g_id):
-    session = vk.Session()
-    api = vk.API(session)
-    offset = 1
-    group_id = '-' + g_id
-    group_posts = api.wall.get(owner_id=group_id, offset=offset, count=5)
-
-    for group_post in group_posts[1:len(group_posts)]:
-        try:
-            text = group_post.get('text')
-            id = group_post.get('id')
-            if id in [post.id for post in Post.select()]:
-                continue
-            for attachment in group_post.get('attachments'):
-
-                if attachment.get('type') == 'photo':
-                    pic = attachment.get('photo').get('src_big')
-                    pic_id = attachment.get('photo').get('pid')
-                    print(pic)
-                    # download(pic, id, pic_id)
-                else:
-                    continue
-            post_comments = api.wall.getComments(owner_id=group_id, post_id=id, need_likes=True)
-            for i in range(len(post_comments) - 1):
-                comment = post_comments[1:len(post_comments)][i]
-                if comment.get('likes').get('count') > 1:
-                    if 'https://vk.com' in comment.get('text'):
-                        continue
-                    else:
-                        if not '[id' in comment.get('text'):
-                            Comments.create(text=comment.get('text'), post_id=id)
-                        else:
-                            continue
-
-            Post.create(text=text, id=id, group_id=g_id)
-        except Exception as err:
-            print(err)
-            continue
-
-
 # Конструкция для дебага
 if __name__ == '__main__':
     get_all_content('101854455')
